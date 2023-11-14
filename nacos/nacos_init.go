0d515be2d46aa91597f4ccf5e97ac28031cb13e8
朱广锋 <12827061+zgf1163391068@user.noreply.gitee.com>
2023-11-14 11:00:00 +0800

    nacos get config

---
+++ nacos/nacos_init.go
@@ -0,0 +1,51 @@
+package nacos
+
+import (
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+)
+
+type NacosClient struct {
+	Client config_client.IConfigClient
+}
+
+func CreateNacosClient(namespaceId string, logDir, cacheDir string) (*NacosClient, error) {
+
+	clientConfig := constant.ClientConfig{
+		NamespaceId:         namespaceId,
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              logDir,
+		CacheDir:            cacheDir,
+		LogLevel:            "debug",
+	}
+
+	serviceConfigs := []constant.ServerConfig{
+		{
+			IpAddr:      "122.51.59.109",
+			ContextPath: "/nacos",
+			Port:        8848,
+			Scheme:      "http",
+		},
+	}
+
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serviceConfigs,
+		},
+	)
+
+	return &NacosClient{
+		Client: client,
+	}, err
+}
+
+func (c *NacosClient) GetConfig(dataId, group string) (string, error) {
+	return c.Client.GetConfig(vo.ConfigParam{
+		DataId: dataId,
+		Group:  group,
+	})
+}
