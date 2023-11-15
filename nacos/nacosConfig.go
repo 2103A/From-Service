98274d5b2454254d923b16b02197447cba235ea9
tangjianlin <12842701+tangjianlin153@user.noreply.gitee.com>
2023-11-15 16:34:37 +0800

    Initial commit

--- nacos/nacosConfig.go
+++ nacos/nacosConfig.go
@@ -2,7 +2,6 @@ package nacos
 
 import (
 	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/vo"
 )
@@ -12,18 +11,14 @@ const (
 	nacosPort = 30655
 )
 
-type NacosConfig struct {
-	NacosClient config_client.IConfigClient
-}
-
-func CreateNacosClient(namespaceId string, logDir, cacheDir string) *NacosConfig {
+func CreateNacosClient(namespaceId string, dataId, group string) (string, error) {
 
 	clientConfig := constant.ClientConfig{
 		NamespaceId:         namespaceId,
 		TimeoutMs:           5000,
 		NotLoadCacheAtStart: true,
-		LogDir:              logDir,
-		CacheDir:            cacheDir,
+		LogDir:              "tmp/nacos/log",
+		CacheDir:            "tmp/nacos/cache",
 		LogLevel:            "debug",
 	}
 
@@ -44,22 +39,11 @@ func CreateNacosClient(namespaceId string, logDir, cacheDir string) *NacosConfig
 	)
 	if err != nil {
 		panic("get nacos client false" + err.Error())
-		return nil
+		return "", err
 	}
 
-	return &NacosConfig{
-		NacosClient: client,
-	}
-
-}
-
-func (n *NacosConfig) GetConfigInfo(dataId, group string) (string, error) {
-	content, err := n.NacosClient.GetConfig(vo.ConfigParam{
+	return client.GetConfig(vo.ConfigParam{
 		DataId: dataId,
 		Group:  group,
 	})
-	if err != nil {
-		return "", err
-	}
-	return content, nil
 }
